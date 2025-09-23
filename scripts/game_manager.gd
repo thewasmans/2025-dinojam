40b7c74cdd2a6c5d14be0c6da729302358b077a8
Thewasmans <96111319+Weshmangle@users.noreply.github.com>
2025-09-23 23:37:27 +0200

    Detect when player hit creature

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -12,6 +12,8 @@ func _ready()->void:
 	_game_ui_death.visible = false
 	death_area.player_death.connect(_on_player_death)
 	_game_ui_death.button_restart.pressed.connect(procedure)
+	
+	get_creatures()
 
 func _on_player_death()->void:
 	Engine.time_scale = 0
@@ -20,3 +22,11 @@ func _on_player_death()->void:
 func procedure()->void:
 	get_tree().reload_current_scene()
 	Engine.time_scale = 1
+	
+func get_creatures()->void:
+	var creatures:Array[Node] = get_tree().get_nodes_in_group("creatures")
+	
+	for creature in creatures:
+		if creature is CreatureDino:
+			creature.character_entered.connect(_on_player_death)
+	
