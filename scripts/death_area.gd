1e194e2e3c7b9dfc785a8164e1817c92e30845ca
Thewasmans <96111319+Weshmangle@users.noreply.github.com>
2025-09-23 16:35:54 +0200

    Fix error for reload scene
    The releod scene moved on signal to avoid error

--- scripts/death_area.gd
+++ scripts/death_area.gd
@@ -1,6 +1,8 @@
 class_name DeathArea
 extends Area3D
 
+signal player_death
+
 @export var position_area:Vector3
 @export var scale_area:Vector3
 @export var model_3d:Node3D
@@ -9,6 +11,8 @@ extends Area3D
 func _ready()->void:
 	model_3d.visible = debug
 
-func _on_body_entered(body:CollisionObject3D)->void:
-	if body as PlayerCharacter:
-		get_tree().reload_current_scene()
+func _on_body_entered(body:Node3D)->void:
+	if body as PlayerCharacter : player_death.emit()
+
+func _on_player_death()->void:
+	get_tree().reload_current_scene()
